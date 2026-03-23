6fbbe2bcab5749d3cc08b22931879db07c8939e7
Matt-a01 <22749@virtual.utsc.edu.mx>
2026-03-23 02:18:07 -0600

    Fix logo

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center gap-2 cursor-pointer">
-          <img src="/src/assets/log.png" alt="Logo de Planeta Huella" className="h-10 w-auto" />
+          <img src="/log.png" alt="Logo de Planeta Huella" className="h-10 w-auto" />
             <h1 className="text-xl font-extrabold text-emerald-400 tracking-tight">
               Planeta Huella
             </h1>
